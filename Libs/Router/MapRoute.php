3d0df4a9b88296ecd0a0500a5095bf695b0c31bd
Sebastian <Sebastian@192.168.4.100>
2012-10-15 13:03:34 -0300

    Me fui a linux, el window me canso

---
+++ Libs/Router/MapRoute.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Mapper
+ *
+ * @author Sebastian
+ */
+
+namespace Libs\Router;
+
+class MapRoute {
+    /**
+     * Array content a collection of routes;
+     * @var Array 
+     */
+    private $mapRoute = [];
+
+    public function __construct() {
+        ;
+    }
+    
+    /**
+     * 
+     * @param Array
+     * @return boolean return true which route 
+     * is a valid route, false in other case
+     */
+    private function validateRoute($route){
+        
+    }
+
+
+    public function add(){
+        
+    }
+
+}
+
+?>
